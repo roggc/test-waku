1af0f863b8fc269cebcf9c88912e1ef7c7d554c4
roggc <roggc9@gmail.com>
2025-03-21 15:42:45 +0100

    comment

--- src/components/say-hello.tsx
+++ src/components/say-hello.tsx
@@ -1,5 +1,5 @@
 //src/components/say-hello.tsx
-//"use client"; //<--- Commenting this line, it doesn't fail on deploy/build
+"use client"; //<--- Commenting this line, it doesn't fail on deploy/build, and the app works fine. Without commenting this line, the app works fine in local, but fails on deploy/build
 
 import SuspenseWithUse from "./suspense-with-use";
 
